19d564f996101fe1df27180fb5e826a525e1c374
Stuart Izon <stuart.izon@gmail.com>
2023-10-11 09:48:26 -0400

    Setup react with simple dev server

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path')
+
+module.exports = {
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public')
+    }
+  },
+  module: {
+    rules: [{ test: /\.tsx$/, use: 'ts-loader' }]
+  }
+}
